f855f377cc57fd9ac14aebe3d44f1b31d0fb579b
Kievv <67612839+Kievv@users.noreply.github.com>
2022-09-22 21:11:37 -0300

    Revert "Minha parte"

--- Trabalho/Trabalho-back-end-main/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const indexController = require('../controllers/indexController')
-
-const router = express();
-
-router.get('/', indexController.mensagemAPI);
-
-module.exports = router;
