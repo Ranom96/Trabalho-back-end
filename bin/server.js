9b4ffe2ca696690a3a756c0173eb8a4fde8f0637
Kievv <ievsouza622@gmail.com>
2022-09-27 16:33:56 -0300

    Swagger Detran atualizado

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,19 @@
-const app = require('../src/app.js')
+const app = require('../src/app.js');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../src/swagger.json');
+
+
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+
+
 
 const port = 3000;
 
 app.listen(port, () => {
     console.log('API está online!')
+
 });
