d335389619dbcec8d563a49bc2c2dd60c3824d26
Kievv <ievsouza622@gmail.com>
2022-11-07 18:29:32 -0300

    mudanças

--- src/controllers/pmController.js
+++ src/controllers/pmController.js
@@ -1,51 +1,47 @@
 const Carro = require('../models/carroModel.js');
 
-class PMController{
+class PMController {
+  static async listarCarros(req, res) {
+    await Carro.find({})
+      .then((carros) => {
+        return res.status(200).json(carros);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  }
 
-    static async listarCarros = (req, res) => {
-        await Carro.find({})
-        .then((carros) => {
-            return res.status(200).json(carros);
-        })
-        .catch((error) => {
-            return res.status(500).json(error);
-          });
-        
-    }
-    
-    static async listarCarrosPorId = (req, res) => {
-        await Carro.findOne({placa: req.params.id})
-        .then((carro) => {
-            if (carro) {
-              return res.json(carro);
-            } else {
-              return res.status(404).json('Carro não localizado');
-            }
-          })
-          .catch((error) => {
-            return res.status(500).json(error);
-          });
-    }
-    
-    static async atualizarCarro = (req, res) => {
-        await Carro.findOneAndUpdate({placa: req.params.id}, req.body,
-        {runValidators: true})
-        .then((carro) => {
-            if (carro) {
-              return res.status(204).end();
-            } else {
-              return res.status(404).json('Carro não localizado');
-            }
-          })
-          .catch((error) => {
-            const msgErro = {};
-            Object.values(error.errors).forEach(({ properties }) => {
-              msgErro[properties.path] = properties.message;
-            });
-            return res.status(422).json(msgErro);
-          });
-    }
-    
+  static async listarCarrosPorId(req, res) {
+    await Carro.findOne({ placa: req.params.id })
+      .then((carro) => {
+        if (carro) {
+          return res.json(carro);
+        } else {
+          return res.status(404).json('Carro não localizado');
+        }
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  }
+
+  static async atualizarCarro(req, res) {
+    await Carro.findOneAndUpdate({ placa: req.params.id }, req.body, { runValidators: true })
+      .then((carro) => {
+        if (carro) {
+          return res.status(204).end();
+        } else {
+          return res.status(404).json('Carro não localizado');
+        }
+      })
+      .catch((error) => {
+        const msgErro = {};
+        Object.values(error.errors).forEach(({ properties }) => {
+          msgErro[properties.path] = properties.message;
+        });
+        return res.status(422).json(msgErro);
+      });
+  }
 }
 
 module.exports = PMController;
--- src/routes/pc.js
+++ src/routes/pc.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 const express = require('express');
-const pcController = require('../controllers/pcController')
+const pcController = require('../controllers/pcController');
 
 const router = express.Router();
 
@@ -10,23 +9,4 @@ router.get('/pc/:id', pcController.listarCarrosPorId);
 
 router.put('/pc/:id', pcController.atualizarCarro);
 
-
-
-
-=======
-const express = require('express');
-const pcController = require('../controllers/pcController')
-
-const router = express.Router();
-
-router.get('/pc', pcController.listarCarros);
-
-router.get('/pc/:id', pcController.listarCarrosPorId);
-
-router.put('/pc/:id', pcController.atualizarCarro);
-
-
-
-
->>>>>>> 1aec79b4c283399ec38c3fc140e1eba5a3947c2b
 module.exports = router;
